4358f20b44afb05de10e4acf279329abd701132f
KalabGebru <123607580+KalabGebru@users.noreply.github.com>
2023-04-25 12:24:12 +0300

    first commit
    upload the locally created vite+react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen w-full">
+        <div className="h-52 w-96 bg-green-300 text-white">yesss</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
