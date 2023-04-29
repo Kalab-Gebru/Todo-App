e3a3e7cad4304ffa48ca5b8b377f931ac0d140dc
KalabGebru <kalabgebru3@gmail.com>
2023-04-29 10:54:41 +0300

    New branch to add  firebase functionality

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNuRkewFaT2SfLwcOLQaqoTuL2KYa3xmE",
+  authDomain: "react-todo-app-b4741.firebaseapp.com",
+  projectId: "react-todo-app-b4741",
+  storageBucket: "react-todo-app-b4741.appspot.com",
+  messagingSenderId: "515438428282",
+  appId: "1:515438428282:web:66763ffe0a87841833bbc3",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
