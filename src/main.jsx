19e952d000fc3fc0d4cedd569e47bab5874f0287
KalabGebru <kalabgebru3@gmail.com>
2023-08-23 15:05:43 +0300

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import { LanguageProvider } from "./contexts/LanguageContext";
 import { DataProvider } from "./hooks/useContextData";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DataProvider>
       <ThemeProvider>
-        <LanguageProvider>
-          <App />
-        </LanguageProvider>
+        <App />
       </ThemeProvider>
     </DataProvider>
   </React.StrictMode>
